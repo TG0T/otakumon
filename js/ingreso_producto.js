b829d46b166414db62d806b9057e5f92d97c8e33
TG0T <134341204+TG0T@users.noreply.github.com>
2024-05-09 20:50:37 -0400

    correccion ingreso productos

--- js/ingreso_producto.js
+++ js/ingreso_producto.js
@@ -14,7 +14,7 @@ $(document).ready(function() { //funcion anonima
         let precio = $('#precio-producto').val()
         let mail = $('#correo-ing').val()
 
-        let url = "https://programadormaldito.cl/route/usuario_duoc_almacenar"
+        let url = "https://programadormaldito.cl/route/producto_duoc_almacenar"
         let datos = {
             codigo: codigo,
             nombre: nombre,
@@ -29,8 +29,28 @@ $(document).ready(function() { //funcion anonima
             contentType: "application/json",
             data: JSON.stringify(datos), //transforma en strings los datos conseguidos
 
+            
+
             success: function(response){ //accion si la operacion funciona
                 console.log(response)
+
+                if(response[0].RESPUESTA == "NOK"){
+            } else {
+                Swal.fire({
+                    title: "Codigo ya en uso",
+                    icon: "error"
+                }
+                );
+                Swal.fire({
+                    title: "Producto Guardado con exito",
+                    icon: "success"
+                }
+                );
+
+                $('#codigo-producto').val('')
+                $('#nombre-producto').val('')
+                $('#descripcion-producto').val('')
+                $('#precio-producto').val('')}
             },
             error: function(status, error){ //accion si la operacion falla
                 console.log(error + " " + status)
