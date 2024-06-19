db613cc513e8ec8d89d3ce6e5a6854b9a181133a
TG0T <134341204+TG0T@users.noreply.github.com>
2024-06-18 21:02:08 -0400

    cositas

--- Otakumon-D/Otakumon/otaku/views.py
+++ Otakumon-D/Otakumon/otaku/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+def index(request):
+    return render(request, "otaku/index.html")
+
+def registro(request):
+    return render(request, "otaku/registro.html")
